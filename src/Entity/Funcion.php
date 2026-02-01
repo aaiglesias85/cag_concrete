831b1cd65a9080a602b4cd6328302b0f495c4208
Andres <aaiglesias85@gmail.com>
2026-01-31 20:38:04 -0600

    nombre en  invoices tabla

--- src/Entity/Funcion.php
+++ src/Entity/Funcion.php
@@ -5,43 +5,43 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: 'App\Repository\FuncionRepository')]
-#[ORM\Table(name: 'function')]
+#[ORM\Table(name: '`function`')]
 class Funcion
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(name: 'function_id', type: 'integer')]
-    private ?int $funcionId;
+   #[ORM\Id]
+   #[ORM\GeneratedValue]
+   #[ORM\Column(name: 'function_id', type: 'integer')]
+   private ?int $funcionId;
 
-    #[ORM\Column(name: 'url', type: 'string', length: 255, nullable: false)]
-    private ?string $url;
+   #[ORM\Column(name: 'url', type: 'string', length: 255, nullable: false)]
+   private ?string $url;
 
-    #[ORM\Column(name: 'description', type: 'string', length: 255, nullable: false)]
-    private ?string $descripcion;
+   #[ORM\Column(name: 'description', type: 'string', length: 255, nullable: false)]
+   private ?string $descripcion;
 
-    public function getFuncionId(): ?int
-    {
-        return $this->funcionId;
-    }
+   public function getFuncionId(): ?int
+   {
+      return $this->funcionId;
+   }
 
-    public function getUrl(): ?string
-    {
-        return $this->url;
-    }
+   public function getUrl(): ?string
+   {
+      return $this->url;
+   }
 
-    public function setUrl(?string $url): void
-    {
-        $this->url = $url;
-    }
+   public function setUrl(?string $url): void
+   {
+      $this->url = $url;
+   }
 
-    public function setDescripcion(?string $descripcion): self
-    {
-        $this->descripcion = $descripcion;
-        return $this;
-    }
+   public function setDescripcion(?string $descripcion): self
+   {
+      $this->descripcion = $descripcion;
+      return $this;
+   }
 
-    public function getDescripcion(): ?string
-    {
-        return $this->descripcion;
-    }
+   public function getDescripcion(): ?string
+   {
+      return $this->descripcion;
+   }
 }
