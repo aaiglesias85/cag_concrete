3c8813ce3bebdddf7b0e4cbbe0eb57583d7128bd
Andres <aaiglesias85@gmail.com>
2026-04-20 19:59:48 -0500

    fix bug parameter 'code' data t
    DataTables warning: table id=subcontracts-table-editable - Requested unknown parameter 'code' for row 0, column 1. For more information about this error, please see https://datatables.net/tn/4

--- public/bundles/metronic8/js/pages/data-tracking.js
+++ public/bundles/metronic8/js/pages/data-tracking.js
@@ -3824,6 +3824,7 @@ var DataTracking = (function () {
                   subcontractor_id: subcontractor_id,
                   subcontractor: subcontractor,
                   project_item_id: project_item_id,
+                  code: item.code,
                   item: item.item,
                   unit: item.unit,
                   quantity: quantity,
@@ -3841,6 +3842,7 @@ var DataTracking = (function () {
                   subcontracts[posicion].subcontractor_id = subcontractor_id;
                   subcontracts[posicion].subcontractor = subcontractor;
                   subcontracts[posicion].project_item_id = project_item_id;
+                  subcontracts[posicion].code = item.code || '';
                   subcontracts[posicion].item = item.item;
                   subcontracts[posicion].unit = item.unit;
                   subcontracts[posicion].quantity = quantity;
